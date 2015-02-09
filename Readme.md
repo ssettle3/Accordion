142a0d22382a5083042a74be68f6c60502589ebf
Stephen Settle <ssettle3@gmail.com>
2015-02-09 15:53:22 -0500

    Edit Readme File

--- Readme.md
+++ Readme.md
@@ -1,2 +1,14 @@
-Assignment 8 @ TIY
-Creating an 'Accordion' using jQuery
+# Accordion
+
+## Description
+ * Using jQuery, JavaScript, HTML and CSS I created a typical 'accordion' with Ipsums loaded in as content.
+
+## Want my Accordion?
+ * Clone my repo
+ * Go To: http://ssettle3.github.io/Accordion/
+
+## Libraries
+ * jQuery
+
+## Copyright / License
+ * This project belongs to Stephen Settle
---
+++ bonusFunc.js
@@ -0,0 +1,22 @@
+// As further practice with JS Arrays, loops and functions, rebuild the following methods using only a for loop.
+//
+// map()
+// reduce()
+// filter()
+
+Array.myMap = function(callBack, i, a){
+	var newArray = [];
+	for(var i = 0; i < arg; i++){
+		newArray.push(arg, i, thisN)
+	};
+
+	return newArray;
+};
+
+Array.myReduce = function(inital, next){
+	var newArray = [];
+	for(var i = 0; i , i++){
+		
+	}
+
+}
